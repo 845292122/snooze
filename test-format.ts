eb90b6a7fed88db267f9d919ec9dd1aa2240de91
Edison.Cheung <845292122@qq.com>
2025-12-08 21:50:32 +0800

    chore: init

---
+++ test-format.ts
@@ -0,0 +1,16 @@
+const testString = 'double quotes'
+const testArray = [1, 2, 3]
+const testObject = { a: 1, b: 2, c: 3 }
+
+const arrowFunc = x => x * 2
+
+function testFunction() {
+  console.log('single quotes')
+  const unused = 'test'
+  return {
+    name: 'test',
+    value: 123
+  }
+}
+
+export default testFunction
